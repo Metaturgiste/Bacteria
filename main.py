9eaf3418e4c6f0edcbd1254aef39b386f18938d1
Metaturgiste <baptiste.brunet-de-la-charie4@etu.univ-lorraine.fr>
2021-12-26 21:26:32 +0100

    *

--- main.py
+++ main.py
@@ -691,6 +691,7 @@ def benchmark():
 
 def no_repeat(n, t, e, f):
     file_exists = exists("results_N_" + str(N) + "_T_" + str(T) + "_optimized_on_" + F + "_tested_on_" + "0" + ".txt")
+    print(file_exists)
     return file_exists
 
 def main(i, j, k):
