b4a829cff4c1be3da5478dd782e0b29a06ef3de3
Metaturgiste <baptiste.brunet-de-la-charie4@etu.univ-lorraine.fr>
2022-02-28 01:24:42 +0100

    *

--- Mini projet/plenty/lala.sh
+++ Mini projet/plenty/lala.sh
@@ -1,3 +1,3 @@
-for i in 2 -1.5 -1.25 -1 -0.75 -0.5 -0.25 0 0.25 0.5 0.75 1 1.25 1.5 2; do for i in 2 -1.5 -1.25 -1 -0.75 -0.5 -0.25 0 0.25 0.5 0.75 1 1.25 1.5 2; do 
+for i in {-2..2..0.1}; do for i in {-2..2..0.1}; do 
 screen -d -m -S w$i$j bash -c "python3 main.py $i $j" #& 
 done; done;
